593fc1d13c4aeebcb6a0bf234a0e828ab0f9e0b2
Alexander-Blair <zanderb@hotmail.co.uk>
2017-09-06 12:38:33 +0100

    Removed unnecessary test and method from game (players), added test to return current player

--- jasmine/spec/game-spec.js
+++ jasmine/spec/game-spec.js
@@ -1,21 +1,57 @@
 describe("Game", function() {
   'use strict';
 
-  var game, mockBoard, randomLine, randomField;
+  var game, mockBoard, mockPlayers, mockTurnCounter;
 
+  mockPlayers = { 0: {}, 1: {} };
+  mockTurnCounter = jasmine.createSpyObj('turnCounter', ['turnNumber', 'increment']);
   mockBoard = jasmine.createSpyObj('board', ['takeField', 'isFull']);
-  game = new Game(mockBoard);
+  game = new Game(mockBoard, mockPlayers, mockTurnCounter);
 
   it("is created with a board", function() {
     expect(game.board()).toEqual(mockBoard);
   });
 
-  describe("#takeField", function() {
+  describe("#player", function() {
+    var randomPlayerNumber = randomBetween(0, 1);
+    
+    it("returns a player object", function() {
+      expect(game.player(randomPlayerNumber))
+        .toEqual(mockPlayers[randomPlayerNumber]);
+    });
+  });
+
+  describe("#currentPlayer", function() {
+    var randomEvenNumber, randomOddNumber;
+
+    randomEvenNumber = randomBetween(0, 4) * 2;
+    it("returns first player if turn is even", function() {
+      mockTurnCounter.turnNumber.and.returnValue(randomEvenNumber);
+      expect(game.currentPlayer()).toEqual(mockPlayers[0]);
+    });
+
+    randomOddNumber = randomEvenNumber + 1;
+    it("returns second player if turn is even", function() {
+      mockTurnCounter.turnNumber.and.returnValue(randomOddNumber);
+      expect(game.currentPlayer()).toEqual(mockPlayers[1]);
+    });
+  });
+
+  describe("#play", function() {
+    var randomLine, randomField;
+
+    randomLine = randomBetween(0, 2);
+    randomField = randomBetween(0, 2);
+
     it("can ask the board to take a specific field", function() {
-      randomLine = randomBetween(0, 2);
-      randomField = randomBetween(0, 2);
-      game.takeField(randomLine, randomField);
+      game.play(randomLine, randomField);
       expect(mockBoard.takeField).toHaveBeenCalledWith(randomLine, randomField);
+    });
+
+    it("asks the turn counter to move to the next round", function() {
+      mockBoard.takeField.and.returnValue(true);
+      game.play(randomLine, randomField);
+      expect(mockTurnCounter.increment).toHaveBeenCalled();
     });
   });
 
--- src/game.js
+++ src/game.js
@@ -1,21 +1,36 @@
 (function(exports) {
   'use strict';
 
-  function Game(board) {
+  function Game(board, players, turnCounter) {
     this._board = board;
+    this._players = players;
+    this._turnCounter = turnCounter;
   }
 
   Game.prototype = {
     board: function() {
       return this._board;
     },
-    takeField: function(lineNumber, fieldNumber) {
-      return this.board().takeField(lineNumber, fieldNumber);
+    player: function(number) {
+      return this._players[number];
+    },
+    play: function(lineNumber, fieldNumber) {
+      if(this.board().takeField(lineNumber, fieldNumber)) {
+        this._turnCounter.increment();
+      }
     },
     isOver: function() {
       return this._board.isFull();
+    },
+    currentPlayer: function() {
+      var playerNumber = isEven(this._turnCounter.turnNumber()) ? 0 : 1;
+      return this.player(playerNumber);
     }
   };
+
+  function isEven(n) {
+    return n % 2 == 0;
+  }
 
   exports.Game = Game;
 })(this);
